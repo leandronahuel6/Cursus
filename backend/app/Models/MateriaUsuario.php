8c0a4179ffa1e9060d83d2100841001d9366b528
ValenReina <valentinrey12@gmail.com>
2026-06-19 16:58:35 -0300

    feat: cree tabla cuota y nota los modelos y controladores de ambos, cargue datos de la cuota actual

--- backend/app/Models/MateriaUsuario.php
+++ backend/app/Models/MateriaUsuario.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class MateriaUsuario extends Model
 {
-    //
+    protected $table = 'materia_usuario';
+
+    protected $fillable = [
+        'usuario_id',
+        'materia_id',
+        'estado_historico',
+        'cursando_actualmente',
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
+
+    public function materia()
+    {
+        return $this->belongsTo(Materia::class);
+    }
+
+    public function notas()
+    {
+        return $this->hasMany(Nota::class);
+    }
 }
--- backend/database/migrations/2026_06_12_131831_create_materia_usuario_table.php
+++ backend/database/migrations/2026_06_12_131831_create_materia_usuario_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void{
         Schema::create('materia_usuario', function (Blueprint $table) {
-        $table->foreignId('usuario_id')->constrained('users')->onDelete('cascade');
-        $table->foreignId('materia_id')->constrained('materias')->onDelete('cascade');
-        $table->enum('estado_historico', ['libre', 'regular', 'aprobada'])->default('libre');
-        $table->boolean('cursando_actualmente')->default(false);
-        $table->primary(['usuario_id', 'materia_id']);
-        $table->timestamps();
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('materia_id')->constrained('materias')->onDelete('cascade');
+            $table->enum('estado_historico', ['libre', 'regular', 'aprobada'])->default('libre');
+            $table->boolean('cursando_actualmente')->default(false);
+            $table->unique(['usuario_id', 'materia_id']);
+            $table->timestamps();
         });
-    }
+}
 
     public function down(): void{
         Schema::dropIfExists('materia_usuario');
