cc1a79d05e7032487fcab2103c92facde3c2a871
ValenReina <valentinrey12@gmail.com>
2026-06-22 23:51:27 -0300

    feat: Cambiar clave desde el perfil,mejoras varias en autenticacion y icono de perfil para version mobile funcional

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\AlertaController;
 use App\Http\Controllers\RecordatorioCuotaController;
 use App\Http\Controllers\PagoCuotaController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
+Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:5,1');
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->middleware('throttle:5,1');
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->middleware('throttle:5,1');
 Route::get('/materias', [MateriaController::class, 'index']);
 Route::get('/cuotas', [CuotaController::class, 'index']);
 
