16dff3054dc75fdbebb5aa0cb450d26c6bff2585
Tiago Castelo <tiagojcastelo@gmail.com>
2026-07-31 12:19:23 -0300

    refactor: unifica el sistema de animaciones y limpia estilos prototipados
    Se consolidaron múltiples animaciones redundantes para utilizar un único conjunto global de clases de animación. También se inyectó el módulo global de animaciones en las vistas independientes y se purgaron los estilos duplicados de fondos para asegurar la herencia global.

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="{{ asset('css/base/fonts.css') }}">
     <link rel="stylesheet" href="{{ asset('css/base/variables.css') }}">
     <link rel="stylesheet" href="{{ asset('css/base/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/base/animations.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="icon" href="{{ asset('assets/icons/cursus-logo.svg') }}" type="image/svg+xml">
     <script src="https://unpkg.com/lucide@latest"></script>
